a0f1d47fee3ecbd62dc688299286782f997e00bd
Rishal <Rishal.mhd009@gmail.com>
2023-06-14 01:40:02 +0530

    need to setup routter

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <Fragment>
+         <footer className="footer has-background-black">
+        <p>&copy; 2023 Paw Mart</p>
+      </footer>
+    </Fragment>
+  )
+}
+
+export default Footer
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,19 +1,43 @@
 import React, { Fragment } from "react";
-import classes from './HomePage.module.css'
+import classes from "./HomePage.module.css";
+import Footer from "./Footer";
+import { Link } from "@mui/material";
+
 
 function HomePage() {
   return (
     <Fragment>
-<div className={classes.maindiv}>
-     
-     
-    </div>
-         <footer>
-        <p>&copy; 2023 Paw Mart</p>
-      </footer>
+      <div className={classes.maindiv}>
+        <div className="columns">
+          <header className={classes.title}>
+            <h3 className=" is-size-2 has-text-left has-text-weight-bold has-text-white ml-6 mt-5">
+              Paw Mart
+            </h3>
+          </header>
+          <div
+            className={`column ${classes.btn} mt-5 is-flex is-justify-content-flex-end is-align-items-flex-start mr-6`}
+          >
+            <div className="buttons">
+              <Link className="button is-primary mr-5">
+                <strong>Sign up</strong>
+              </Link>
+              <Link className="button is-light mr-5">Log in</Link>
+            </div>
+          </div>
+        </div>
+        <div className={`main ${classes.centered}`}>
+          <h4 className="is-size-4 has-text-weight-bold has-text-white">Welcome to Paw Mart</h4>
+          <p className="lead has-text-white is-size-5">
+            Discover a world of wagging tails and purrs at our Pet
+            Shop. We offer a wide range of lovable pets. Find your perfect furry
+            companion and provide them with the love and care they deserve.
+          </p>
+          <Link className="button mt-5 is-size-4">View Pets</Link>
+        </div>
+      </div>
+
+      <Footer />
     </Fragment>
-    
-   
   );
 }
 
