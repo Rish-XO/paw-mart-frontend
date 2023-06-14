dbeb61de7a4cfb69aab67cc8f03a4b4a27eeba09
Rishal <Rishal.mhd009@gmail.com>
2023-06-14 07:42:04 +0530

    working on navbar

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import { Container, Typography } from "@mui/material";
+import React from "react";
+import PetCard from "./layout/PetCard";
 
 function Posts() {
   return (
-    <div>
-      <h1>posts</h1>
-      <img src="https://images.unsplash.com/photo-1548940740-204726a19be3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2578&q=80" alt="" />
-    </div>
-  )
+    <Container sx={{
+     height: "100vh",
+     display: "flex",
+     flexDirection: "column",
+     alignContent: 'center',
+     alignItems: "center"
+      
+    }}>
+      <Typography variant="h1" sx={{
+      }}>Browse your Pets</Typography>
+     
+      <PetCard />
+    </Container>
+  );
 }
 
-export default Posts
+export default Posts;
---
+++ src/components/layout/PetCard.js
@@ -0,0 +1,73 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import { Grid, Typography } from "@mui/material";
+
+function PetCard() {
+  return (
+    <div className="crd">
+        <Card sx={{ display: "flex" }}>
+          <CardMedia
+            sx={{ width: 400, height: 400 }}
+            image="https://images.unsplash.com/photo-1592754862816-1a21a4ea2281?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2574&q=80"
+            title="green iguana"
+          />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+            }}
+          >
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </div>
+        </Card>
+
+      <Card sx={{ display: "flex" }}>
+        <CardMedia
+          sx={{ width: 400, height: 400 }}
+          image="https://images.unsplash.com/photo-1592754862816-1a21a4ea2281?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2574&q=80"
+          title="green iguana"
+        />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </div>
+      </Card>
+    </div>
+  );
+}
+
+export default PetCard;
