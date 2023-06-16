32954720d55e521662aabe8492aef3046f02c902
Rishal <Rishal.mhd009@gmail.com>
2023-06-16 16:28:31 +0530

    dummy image

--- src/components/layout/PetCard.js
+++ src/components/layout/PetCard.js
@@ -79,7 +79,7 @@ function PetCard() {
                 alignItems: "center",
                 justifyContent: "center",
               }}
-              image="https://images.unsplash.com/photo-1592754862816-1a21a4ea2281?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2574&q=80"
+              image="https://images.unsplash.com/photo-1592194996308-7b43878e84a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2574&q=80"
               title="green iguana"
             />
             <CardContent>
