3d40ef486462175514bfdad31cae876bb46c1d75
Rishal <Rishal.mhd009@gmail.com>
2023-06-21 23:03:40 +0530

    c

--- src/pages/edit.js
+++ src/pages/edit.js
@@ -76,7 +76,6 @@ const EditForm = () => {
         `http://localhost:5000/posts/${id}/edit`,
         body
       );
-      navigate(-1);
     } catch (error) {
       console.log(error.message);
     }
