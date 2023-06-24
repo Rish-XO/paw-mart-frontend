b16e6943efbb676e7d31fdf30f1bc61a1ed52a16
Rishal <Rishal.mhd009@gmail.com>
2023-06-24 23:22:30 +0530

    c

--- src/utils/store/authSlice.js
+++ src/utils/store/authSlice.js
@@ -1,28 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
-    name: 'authentication',
-    initialState: {
-        isLoggedIn : false,
-        role : null,
-        status: ''
+  name: "authentication",
+  initialState: {
+    isLoggedIn: false,
+    role: null,
+    status: "",
+  },
+  reducers: {
+    loginHandler(state, action) {
+      const data = action.payload;
+      console.log(data);
+      state.isLoggedIn = true;
+      state.role = data.role;
+      localStorage.setItem("token", data.token);
     },
-    reducers: {
-        loginHandler(state, action) {
-            const data = action.payload
-            console.log(data);
-            state.isLoggedIn = true;
-            state.role = data.role
-            localStorage.setItem('token', data.token)
-        },
-        logoutHandler(state, action) {
-            console.log('logging out');
-            state.isLoggedIn = false;
-            state.role = null;
-            localStorage.removeItem('token')
-        }
-    }
-})
+    logoutHandler(state, action) {
+      console.log("logging out");
+      state.isLoggedIn = false;
+      state.role = null;
+      localStorage.removeItem("token");
+    },
+  },
+});
 
-export const authActions=authSlice.actions;
+export const { loginHandler, logoutHandler } = authSlice.actions;
 export default authSlice.reducer;
