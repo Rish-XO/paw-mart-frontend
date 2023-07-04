c9bcb7f0378bbed27d3441f3243a7c4c21805a3e
Rishal <Rishal.mhd009@gmail.com>
2023-07-05 04:30:29 +0530

    c

--- src/components/Chat/ChatPage.js
+++ src/components/Chat/ChatPage.js
@@ -1,47 +1,54 @@
 import React, { useEffect, useState } from "react";
 import { Box, Container, Grid, Paper, Typography } from "@mui/material";
 import FaceIcon from "@mui/icons-material/Face";
+import CloseIcon from "@mui/icons-material/Close";
 import "./ChatPage.css";
 
 const ChatPage = () => {
   const [selectedChat, setSelectedChat] = useState("");
 
-  useEffect(()=> {
-    const storedChat = localStorage.getItem("selectedChat")
-    if(storedChat) {
-      setSelectedChat(storedChat)
+  useEffect(() => {
+    const storedChat = localStorage.getItem("selectedChat");
+    if (storedChat) {
+      setSelectedChat(storedChat);
     }
-  },[])
+  }, []);
 
   const selectChatHandler = (name) => {
     // console.log(name);
     setSelectedChat(name);
-    localStorage.setItem("selectedChat", name)
+    localStorage.setItem("selectedChat", name);
   };
 
   const chatIsSelected = (name) => {
-    return selectedChat === name ? "selected-chat" : ""
-  }
+    return selectedChat === name ? "selected-chat" : "";
+  };
   return (
     <Container sx={{ marginTop: "5rem" }} className="chat-page">
       <Grid container>
-        <Grid item xs={4}>
+        <Grid item xs={5}>
           <Box className="inbox-header" sx={{ backgroundColor: "#94E8D7" }}>
             <Typography variant="h5" fontWeight="bold">
               INBOX
             </Typography>
           </Box>
-          <Paper sx={{ marginRight: "5px" }} className={`chat-list ${chatIsSelected("Afrin")}`}>
+          <Paper
+            sx={{ marginRight: "5px" }}
+            className={`chat-list ${chatIsSelected("Afrin")}`}
+          >
             <div
-            className="chat-item"
+              className="chat-item"
               onClick={() => selectChatHandler("Afrin")}
             >
               Afrin
             </div>
           </Paper>
-          <Paper sx={{ marginRight: "5px" }} className={`chat-list ${chatIsSelected("Rishal")}`}>
+          <Paper
+            sx={{ marginRight: "5px" }}
+            className={`chat-list ${chatIsSelected("Rishal")}`}
+          >
             <div
-               className="chat-item"
+              className="chat-item"
               onClick={() => selectChatHandler("Rishal")}
             >
               Rishal
@@ -49,12 +56,38 @@ const ChatPage = () => {
             {/* Add more chat items */}
           </Paper>
         </Grid>
-        <Grid item xs={8}>
+
+        {/* the chat writing space */}
+        <Grid item xs={7}>
           <Paper className="chat-content">
-            <Typography variant="h6" className="chat-header">
-              <FaceIcon sx={{ marginRight: "5px" }} />
-              {selectedChat}
-            </Typography>
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+              <Typography variant="h6" className="chat-header">
+                <FaceIcon sx={{ marginRight: "5px" }} />
+                {selectedChat}
+              </Typography>
+              <Box
+                sx={{
+                  width: "2rem",
+                  height: "2rem",
+                  borderRadius: "50%",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  cursor: "pointer",
+                  transition: "background-color 0.3s ease",
+                  "&:hover": {
+                    backgroundColor: "#8EE2D2 ",
+                  },
+                }}
+              >
+                <CloseIcon
+                  sx={{
+                    fontSize: "2rem",
+                    cursor: "pointer",
+                  }}
+                />
+              </Box>
+            </Box>
             <hr></hr>
             <div className="chat-bubble">
               <div className="message">
