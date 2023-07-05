30fdc467f300a56e7a5d9a1c916bbe10f43780ec
Rishal <Rishal.mhd009@gmail.com>
2023-07-05 05:31:03 +0530

    chat filler done

--- src/components/Chat/ChatFiller.js
+++ src/components/Chat/ChatFiller.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react";
 import AnnouncementIcon from "@mui/icons-material/Announcement";
-import { Typography } from '@mui/material';
+import { Typography } from "@mui/material";
 
 function ChatFiller() {
   return (
-    <div className='filler'>
-      <AnnouncementIcon sx={{fontSize: '5rem'}} />
-      <Typography align='center'>
-      Select a chat to view conversation
-      </Typography>
+    <div className="filler">
+      <div className="icon-wrapper">
+        <AnnouncementIcon sx={{ fontSize: "5rem" , margin: "30% 50% 0% 50%"}} />
+      </div>
+      <Typography sx={{marginLeft:"60px"}} align="center">Select a chat to view conversation</Typography>
     </div>
-  )
+  );
 }
 
-export default ChatFiller
+export default ChatFiller;
