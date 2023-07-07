7eb60aa6860b82eafb3f9ceaa7bfc418336eb79f
Rishal <Rishal.mhd009@gmail.com>
2023-07-07 20:14:41 +0530

    c

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ChatPage from '../components/Chat/ChatPage'
 import Navbar from '../components/layout/Navbar'
+import { useNavigate } from 'react-router';
+import { useSelector } from 'react-redux';
 
 function Chat() {
+  const navigate = useNavigate()
+  const isLoggedIn = useSelector((state) => state.authHandler.isLoggedIn);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate("/signup");
+    }
+  }, [isLoggedIn, navigate]);
+
+  
+
   return (
     <div>
       <Navbar />
