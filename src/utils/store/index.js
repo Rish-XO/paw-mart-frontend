522fe6d26b5859b5cbe705abbea1293ea60b98f5
Rishal <Rishal.mhd009@gmail.com>
2023-07-13 00:54:33 +0530

    c

--- src/utils/store/index.js
+++ src/utils/store/index.js
@@ -1,9 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
-import chatSlice from "./chatSlice";
 
 const store = configureStore({
-  reducer: { authHandler: authSlice , messageHandler : chatSlice}
+  reducer: { authHandler: authSlice}
 });
 
 export default store;
