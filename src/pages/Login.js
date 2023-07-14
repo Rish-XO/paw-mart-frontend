1cfeb9530c9db8fd7f0eea3516075c60312d8db4
Rishal <Rishal.mhd009@gmail.com>
2023-07-14 17:04:08 +0530

    c

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -56,7 +56,7 @@ export default function LoginPage() {
       password: data.get("password"),
     };
 
-    try {   
+    try {    
       const response = await axios.post("http://localhost:5000/login", body);
       const { token, role, id } = response.data;
       // console.log(token, role);
