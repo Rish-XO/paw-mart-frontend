27d1e35ef845a6508a56536ab0b754a493ac2199
Rishal <Rishal.mhd009@gmail.com>
2023-07-17 21:26:02 +0530

    c

--- src/components/snackbar/Snackbar.js
+++ src/components/snackbar/Snackbar.js
@@ -2,44 +2,52 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Snackbar from '@mui/material/Snackbar';
-import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import MuiAlert from '@mui/material/Alert';
+import { useDispatch, useSelector } from 'react-redux';
+import { snackActions } from '../../assets/store/snackbarSlice';
 
-const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
-  props,
-  ref,
-) {
+const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
 export default function CustomizedSnackbars() {
-  const [open, setOpen] = React.useState(false);
+//   const [open, setOpen] = React.useState(state);
+//   const [vertical,setVertical]=useState(vertical)
+//   const [horizontal,setHorizontal]=useState(horizontal)
+const dispatch=useDispatch();
+const details=useSelector(state=>state.snackBarHandler)
+console.log(details)
 
-  const handleClick = () => {
-    setOpen(true);
-  };
 
-  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+
+//   const handleClick = () => {
+    // setOpen(true); 
+//   };
+
+  const handleClose = (event, reason) => {
+    console.log(event,reason)
     if (reason === 'clickaway') {
       return;
     }
-
-    setOpen(false);
+    dispatch(snackActions.changeState())
+    // setSnakeState(false)
+    // setOpen(false);
   };
 
   return (
     <Stack spacing={2} sx={{ width: '100%' }}>
-      <Button variant="outlined" onClick={handleClick}>
+      {/* <Button variant="outlined" onClick={handleClick}>
         Open success snackbar
-      </Button>
-      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
-          This is a success message!
+      </Button> */}
+      <Snackbar open={details.state} autoHideDuration={6000} anchorOrigin={details.position} onClose={handleClose}>
+        <Alert onClose={handleClose} severity= {details.severity} sx={{ width: '100%' }}>
+          {details.message}
         </Alert>
       </Snackbar>
-      <Alert severity="error">This is an error message!</Alert>
+      {/* <Alert severity="error">This is an error message!</Alert>
       <Alert severity="warning">This is a warning message!</Alert>
       <Alert severity="info">This is an information message!</Alert>
-      <Alert severity="success">This is a success message!</Alert>
+      <Alert severity="success">This is a success message!</Alert> */}
     </Stack>
   );
 }
