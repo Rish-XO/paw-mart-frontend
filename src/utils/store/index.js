068643f05edf66a05694f6c4de6f017693ab81ac
Rishal <Rishal.mhd009@gmail.com>
2023-07-17 20:42:15 +0530

    n

--- src/utils/store/index.js
+++ src/utils/store/index.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
+import snackbarSlice from "./snackbarSlice";
 
 const store = configureStore({
-  reducer: { authHandler: authSlice}
+  reducer: { authHandler: authSlice, snackbarHandler: snackbarSlice}
 });
 
 export default store;
