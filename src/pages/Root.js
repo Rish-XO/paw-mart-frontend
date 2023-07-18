e95b8e6396da47e92519dd463e1a383b9b73edad
Rishal <Rishal.mhd009@gmail.com>
2023-07-18 23:03:27 +0530

    c

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,7 +1,6 @@
 import { CssBaseline } from '@mui/material'
 import React from 'react'
 import { Outlet } from 'react-router'
-import CustomizedSnackbars from '../components/snackbar/Snackbar'
 
 
 function Root() {
@@ -9,7 +8,7 @@ function Root() {
     <main>
       <CssBaseline />
         <Outlet />
-        <CustomizedSnackbars />
+        
     </main>
   )
 }
