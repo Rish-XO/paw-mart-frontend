a91aca0376db8cf5a4da55dbcdb9bd268773d9c6
Rishal <Rishal.mhd009@gmail.com>
2023-07-18 23:04:47 +0530

    b

--- src/components/snackbar/Snackbar.js
+++ src/components/snackbar/Snackbar.js
@@ -1,37 +1,43 @@
-import * as React from 'react';
-import Stack from '@mui/material/Stack';
-
-import Snackbar from '@mui/material/Snackbar';
-import MuiAlert from '@mui/material/Alert';
-import { useDispatch, useSelector } from 'react-redux';
-import { changeState } from '../../utils/store/snackbarSlice';
-
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import { useDispatch, useSelector } from "react-redux";
+import { changeState } from "../../utils/store/snackbarSlice";
 
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
 export default function CustomizedSnackbars() {
-
-const dispatch=useDispatch();
-const details=useSelector(state=>state.snackbarHandler)
-console.log(details)
+  const dispatch = useDispatch();
+  const details = useSelector((state) => state.snackbarHandler);
+  console.log(details);
 
   const handleClose = (event, reason) => {
-    console.log(event,reason)
-    if (reason === 'clickaway') {
+    console.log(event, reason);
+    if (reason === "clickaway") {
       return;
     }
-    dispatch(changeState())
+    dispatch(changeState());
   };
 
   return (
-    <Stack spacing={2} sx={{ width: '100%' }}>
+    <Stack spacing={2} sx={{ width: "100%" }}>
       {/* <Button variant="outlined" onClick={handleClick}>
         Open success snackbar
       </Button> */}
-      <Snackbar open={details.state} autoHideDuration={6000} anchorOrigin={details.position} onClose={handleClose}>
-        <Alert onClose={handleClose} severity= {details.severity} sx={{ width: '100%' }}>
+      <Snackbar
+        open={details.state}
+        autoHideDuration={6000}
+        anchorOrigin={details.position}
+        onClose={handleClose}
+      >
+        <Alert
+          onClose={handleClose}
+          severity={details.severity}
+          sx={{ width: "100%" }}
+        >
           {details.message}
         </Alert>
       </Snackbar>
