15db1495c22749ac066aa61366d9bb7f5389b5fe
Rishal <Rishal.mhd009@gmail.com>
2023-07-27 00:02:56 +0530

    a

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router";
 import Navbar from "../components/layout/Navbar";
 import { Container, Typography } from "@mui/material";
+import PetCard from "../components/layout/PetCard";
 
 function Profile() {
   const { userID } = useParams();
@@ -12,6 +13,7 @@ function Profile() {
         <Typography sx={{ marginTop: "40px" }} variant="h1">
           your posts
         </Typography>
+        {/* <PetCard /> */}
       </Container>
     </>
   );
