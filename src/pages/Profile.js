2bae8eaf4249ef105add1c6a0006fe7a7db3e4f0
Rishal <Rishal.mhd009@gmail.com>
2023-07-27 00:13:28 +0530

    c

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import Navbar from "../components/layout/Navbar";
 import { Container, Typography } from "@mui/material";
@@ -6,16 +6,19 @@ import PetCard from "../components/layout/PetCard";
 import axios from "axios";
 
 function Profile() {
+  const [pets, setPets] = useState([]);
   const { userID } = useParams();
   const yourPost = async () => {
     try {
       const response = await axios.get(`http://localhost:5000/posts/${userID}`);
-      const pets = response.data
+      setPets(response.data);
     } catch (error) {
       console.log(error.message);
     }
   };
-  useEffect(() => {});
+  useEffect(() => {
+    yourPost();
+  }, [userID]);
   return (
     <>
       <Navbar />
@@ -23,7 +26,7 @@ function Profile() {
         <Typography sx={{ marginTop: "40px" }} variant="h1">
           your posts
         </Typography>
-        <PetCard pets={pets}/>
+        <PetCard pets={pets} />
       </Container>
     </>
   );
