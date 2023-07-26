5b2cff8d2b16c7b4dcc2e1ace91df17bf80a199b
Rishal <Rishal.mhd009@gmail.com>
2023-07-26 23:51:04 +0530

    s

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { useParams } from "react-router";
+import Navbar from "../components/layout/Navbar";
+import { Container, Typography } from "@mui/material";
 
 function Profile() {
   const { userID } = useParams();
   return (
-   <>
-   
-   </>
+    <>
+      <Navbar />
+      <Container>
+        <Typography sx={{ marginTop: "40px" }} variant="h1">
+          your posts
+        </Typography>
+      </Container>
+    </>
   );
 }
 
