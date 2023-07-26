26f7dfd9043149b7ccf0a2da306b39950b7a0647
Rishal <Rishal.mhd009@gmail.com>
2023-07-27 00:08:32 +0530

    c

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router";
 import Navbar from "../components/layout/Navbar";
 import { Container, Typography } from "@mui/material";
 import PetCard from "../components/layout/PetCard";
+import axios from "axios";
 
 function Profile() {
   const { userID } = useParams();
+  const yourPost = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/posts/${userID}`);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  useEffect(() => {});
   return (
     <>
       <Navbar />
