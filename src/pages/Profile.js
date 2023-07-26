2ef49011383cd15e76af796bfac1f7c997299a34
Rishal <Rishal.mhd009@gmail.com>
2023-07-26 23:38:32 +0530

    c

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router'
 
 function Profile() {
+    const user_id = useParams()
   return (
     <div>
-      
+      <h1>helloo 
+        {user_id}
+      </h1>
     </div>
   )
 }
