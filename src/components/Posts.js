ca695f462867902679f750cfd33adfceab58e326
Rishal <Rishal.mhd009@gmail.com>
2023-07-27 00:02:07 +0530

    c

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,22 +1,47 @@
 import { Container, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PetCard from "./layout/PetCard";
-
-
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { snackBarDetailsAdder } from "../utils/store/snackbarSlice";
 
 function Posts() {
+  const dispatch = useDispatch();
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/posts");
+        const data = response.data;
+        setPets(data);
+      } catch (error) {
+        dispatch(
+          snackBarDetailsAdder({
+            severity: "error",
+            message: "Server Error, Try again",
+          })
+        );
+        console.log(error.message);
+      }
+    };
+    getPosts();
+  }, [dispatch]);
+
   return (
-    <Container  sx={{
-     height: "100vh",
-     display: "flex",
-     flexDirection: "column",
-     alignContent: 'center',
-     alignItems: "center"
-      
-    }}>
-      <Typography variant="h1" sx={{ marginBottom : '40px' , marginTop : '60px'}}>Browse your Pets</Typography>
-     
-      <PetCard />
+    <Container
+      sx={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Typography variant="h1" sx={{ marginBottom: "40px", marginTop: "60px" }}>
+        Browse your Pets
+      </Typography>
+      <PetCard pets={pets} />
     </Container>
   );
 }
--- src/components/layout/PetCard.js
+++ src/components/layout/PetCard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -7,88 +7,8 @@ import Button from "@mui/material/Button";
 import { Typography, Grid } from "@mui/material";
 import "./PetCard.css";
 import { Link } from "react-router-dom";
-import axios from "axios";
-import { useDispatch } from "react-redux";
-import { snackBarDetailsAdder } from "../../utils/store/snackbarSlice";
 
-// const pets = [
-//   {
-//     id: 1,
-//     category: "dog",
-//     breed: "Labrador Retriever",
-//     price: 500,
-//     description: "Friendly and energetic. Loves playing fetch.",
-//   },
-//   {
-//     id: 2,
-//     category: "cat",
-//     breed: "Persian",
-//     price: 300,
-//     description: "Independent and affectionate. Requires grooming.",
-//   },
-//   {
-//     id: 3,
-//     category: "dog",
-//     breed: "German Shepherd",
-//     price: 700,
-//     description: "Intelligent and loyal. Excellent for protection.",
-//   },
-//   {
-//     id: 4,
-//     category: "fish",
-//     breed: "Goldfish",
-//     price: 10,
-//     description: "Colorful and low-maintenance. Suitable for beginners.",
-//   },
-//   {
-//     id: 5,
-//     category: "dog",
-//     breed: "Poodle",
-//     price: 600,
-//     description: "Hypoallergenic and highly trainable. Great with kids.",
-//   },
-//   {
-//     id: 6,
-//     category: "cat",
-//     breed: "Siamese",
-//     price: 250,
-//     description: "Vocal and social. Known for their striking blue eyes.",
-//   },
-//   {
-//     id: 7,
-//     category: "bird",
-//     breed: "Parakeet",
-//     price: 50,
-//     description: "Small and playful. Can be taught to mimic words.",
-//   },
-//   {
-//     id: 8,
-//     category: "dog",
-//     breed: "Bulldog",
-//     price: 800,
-//     description: "Calm and friendly. Requires regular exercise.",
-//   },
-// ];
-
-function PetCard() {
-  const dispatch = useDispatch()
-  const [pets, setPets] = useState([]);
-
-  useEffect(() => {
-    // console.log("card loading");
-    const getPosts = async () => {
-      try {
-        const response = await axios.get("http://localhost:5000/posts");
-        const data = response.data;
-        setPets(data);
-      } catch (error) {
-        dispatch(snackBarDetailsAdder({severity: "error", message: "Server Error, Try again"}))
-        console.log(error.message);
-      }
-    };
-    getPosts();
-  }, [dispatch]);
-
+function PetCard({ pets }) {
   return (
     <Grid container spacing={2} justifyContent="center">
       {pets.map((pet) => (
@@ -105,7 +25,7 @@ function PetCard() {
                   display: "flex",
                   alignItems: "center",
                   justifyContent: "center",
-                  objectFit: "contain"
+                  objectFit: "contain",
                 }}
                 image={pet.first_url}
                 title="green iguana"
@@ -120,9 +40,6 @@ function PetCard() {
                 <Typography gutterBottom variant="subtitle2" component="div">
                   {pet.category}
                 </Typography>
-                {/* <Typography variant="body2" color="text.secondary">
-                {pet.description}
-              </Typography> */}
               </CardContent>
               <CardActions>
                 <Link to={`${pet.post_id}`}>
