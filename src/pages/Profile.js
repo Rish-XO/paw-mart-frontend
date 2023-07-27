27d6a18c931eba1cb54b84f8b97c1bf80c7dffee
Rishal <Rishal.mhd009@gmail.com>
2023-07-27 22:54:56 +0530

    s

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -23,7 +23,7 @@ function Profile() {
     <>  
       <Navbar />
       <Container> 
-        <Typography sx={{ marginTop: "40px" , marginBottom: '20px'}} variant="h1">
+        <Typography  sx={{ marginTop: "40px" , marginBottom: '20px'}} variant="h1">
           your posts  
         </Typography> 
         <PetCard pets={pets} /> 
